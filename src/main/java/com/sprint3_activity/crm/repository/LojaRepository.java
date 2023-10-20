8fa8ccf5559b07185e776f1586bf4ce9dd77e8e5
Tiago Filipe Almeida <tfilipal@emeal.nttdata.com>
2023-10-20 10:54:16 +0100

    Loja_Repository

---
+++ src/main/java/com/sprint3_activity/crm/repository/LojaRepository.java
@@ -0,0 +1,9 @@
+package com.sprint3_activity.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sprint3_activity.crm.entity.Loja;
+
+public interface LojaRepository extends JpaRepository<Loja, Integer>{
+
+}
